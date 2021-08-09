bb609931e2d146a125422265dff442f8b333ec9b
vothikimyen108 <vothikinyen2k@gmail.com>
2021-08-09 17:23:13 +0700

    code overlay

--- social-network/src/App.js
+++ social-network/src/App.js
@@ -4,9 +4,24 @@ import "./App.css";
 // import CommentItem from "./components/Comment/CommentItem";
 // import CommentList from "./components/Comment/CommentList";
 
+import React, { useState } from "react";
 import Photo from "./components/Photos/Photo";
 function App() {
-  return <Photo></Photo>;
+  const [openCart, setOpenCart] = useState(false);
+  const handerOnlick = () => {
+    setOpenCart(true);
+    console.log(openCart);
+  };
+  const handerClose = () => {
+    console.log(openCart);
+    setOpenCart(false);
+  };
+  return (
+    <div>
+      <button onClick={handerOnlick}>aaaaa</button>
+      {openCart && <Photo onClose={handerClose}></Photo>}
+    </div>
+  );
 }
 
 export default App;
--- social-network/src/components/Photos/Photo.js
+++ social-network/src/components/Photos/Photo.js
@@ -1,10 +1,10 @@
 import Slider from "react-animated-slider";
 import "react-animated-slider/build/horizontal.css";
 import { makeStyles } from "@material-ui/core/styles";
-import Paper from "@material-ui/core/Paper";
+
 import Grid from "@material-ui/core/Grid";
 import Anh from "../../assets/ImgPost/anh1.jpg";
-import Anh1 from "../../assets/ImgPost/anh2.jpg";
+
 import Anh2 from "../../assets/ImgPost/anh3.jpg";
 import Anh3 from "../../assets/ImgPost/anh4.jpg";
 import NewsItem from "../News/NewsItem";
@@ -12,6 +12,9 @@ import "./styles.css";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import CloseIcon from "@material-ui/icons/Close";
 import IconButton from "@material-ui/core/IconButton";
+
+import Modal from "../UI/Modal";
+
 const useStyles = makeStyles((theme) => ({
   root: {
     backgroundColor: "#000",
@@ -48,38 +51,44 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const Photo = () => {
+const Photo = (props) => {
   const classes = useStyles();
   const slides = [{ img: `${Anh}` }, { img: `${Anh2}` }, { img: `${Anh3}` }];
   return (
-    <Grid container component="main" className={classes.root}>
-      <CssBaseline />
-      <Grid item xs={12} sm={12} md={8} className={classes.left}>
-        <IconButton className={classes.button} component="span">
-          <CloseIcon></CloseIcon>
-        </IconButton>
-        <Slider className="slider-wrapper">
-          {slides.map((slide, index) => (
-            <div key={index} className={classes.root}>
-              <img src={slide.img} className={classes.img} alt="anh"></img>
-            </div>
-          ))}
-        </Slider>
+    <Modal>
+      <Grid container component="main" className={classes.root}>
+        <CssBaseline />
+        <Grid item xs={12} sm={12} md={8} className={classes.left}>
+          <IconButton
+            className={classes.button}
+            component="span"
+            onClick={props.onClose}
+          >
+            <CloseIcon></CloseIcon>
+          </IconButton>
+          <Slider className="slider-wrapper">
+            {slides.map((slide, index) => (
+              <div key={index} className={classes.root}>
+                <img src={slide.img} className={classes.img} alt="anh"></img>
+              </div>
+            ))}
+          </Slider>
+        </Grid>
+        <Grid
+          item
+          xs={12}
+          sm={12}
+          md={4}
+          elevation={6}
+          square
+          className={classes.right}
+        >
+          <NewsItem isOpenCMT={true} isExpanded={false}>
+            {" "}
+          </NewsItem>
+        </Grid>
       </Grid>
-      <Grid
-        item
-        xs={12}
-        sm={12}
-        md={4}
-        elevation={6}
-        square
-        className={classes.right}
-      >
-        <NewsItem isOpenCMT={true} isExpanded={false}>
-          {" "}
-        </NewsItem>
-      </Grid>
-    </Grid>
+    </Modal>
   );
 };
 export default Photo;
