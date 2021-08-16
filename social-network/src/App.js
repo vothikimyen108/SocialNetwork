f21ecbe46a23ddeccb9bf0299a52fab2ed46956a
vothikimyen108 <vothikinyen2k@gmail.com>
2021-08-16 14:31:47 +0700

    fix img

--- social-network/src/App.js
+++ social-network/src/App.js
@@ -1,43 +1,23 @@
-// import Login from "./components/Login/Login";
-//
-// import CommentItem from "./components/Comment/CommentItem";
-// import CommentList from "./components/Comment/CommentList";
-
-// import React, { useState } from "react";
-// import Photo from "./components/Photos/Photo";
-// import NewsForm from "./components/News/NewsForm";
-// import Member from "./components/Members/Member";
-// import MembersList from "./components/Members/MembersList";
-// import PhotoList from "./components/Photos/PhotoList";
-// import { makeStyles } from "@material-ui/core/styles";
-// import Paper from "@material-ui/core/Paper";
+import {
+  ThemeProvider,
+  createTheme,
+  makeStyles,
+} from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import MainNavigation from "./components/Layout/MainNavigation";
-// import HeaderProfile from "./components/Profile/HeaderProfile";
+import NewsFeed from "./pages/NewsFeed";
+
 import SecondNavigation from "./components/Layout/SecondNavigation";
 import LayoutMenber from "./components/Layout/LayoutMenber";
-
-// import ListPhoto from "./components/Profile/ListPhoto";
 import LayoutListMember from "./components/Layout/LayoutListMember";
-// import Anh from "./assets/ImgPost/anh1.jpg";
-// import Anh1 from "./assets/ImgPost/anh2.jpg";
-// import Anh2 from "./assets/ImgPost/anh3.jpg";
-// import Anh3 from "./assets/ImgPost/anh4.jpg";
+const theme = createTheme();
 
-import NewsFeed from "./pages/NewsFeed";
-// import MembersList from "./components/Members/MembersList";
+const useStyles = makeStyles((theme) => {
+  root: {
+    // some css that access to theme
+  }
+});
 
-// import { PeopleItem } from "./components/Members/PeopleItem";
-// const useStyles = makeStyles((theme) => ({
-//   root: {
-//     flexGrow: 1,
-//   },
-//   paper: {
-//     padding: theme.spacing(2),
-//     textAlign: "center",
-//     color: theme.palette.text.secondary,
-//   },
-// }));
 const dataMember = [
   {
     id: 1,
@@ -55,86 +35,29 @@ const dataMember = [
     createJoin: "12 ngày trước",
   },
 ];
-function App() {
-  // const [openCart, setOpenCart] = useState(false);
-  // const handerOnlick = () => {
-  //   setOpenCart(true);
-  //   console.log(openCart);
-  // };
-  // const handerClose = () => {
-  //   console.log(openCart);
-  //   setOpenCart(false);
-  // };
-  // const itemData = [
-  //   {
-  //     img: `${Anh}`,
-  //     title: "Breakfast",
-  //     author: "jill111",
-  //     featured: true,
-  //   },
-  //   {
-  //     img: `${Anh1}`,
-  //     title: "Tasty burger",
-  //     author: "director90",
-  //   },
-  //   {
-  //     img: `${Anh2}`,
-  //     title: "Camera",
-  //     author: "Danson67",
-  //   },
-  //   {
-  //     img: `${Anh3}`,
-  //     title: "Morning",
-  //     author: "fancycrave1",
-  //     featured: true,
-  //   },
-  //   {
-  //     img: `${Anh}`,
-  //     title: "Breakfast",
-  //     author: "jill111",
-  //     featured: true,
-  //   },
-  //   {
-  //     img: `${Anh1}`,
-  //     title: "Tasty burger",
-  //     author: "director90",
-  //   },
-  //   {
-  //     img: `${Anh2}`,
-  //     title: "Camera",
-  //     author: "Danson67",
-  //   },
-  //   {
-  //     img: `${Anh3}`,
-  //     title: "Morning",
-  //     author: "fancycrave1",
-  //     featured: true,
-  //   },
-  // ];
 
+function App() {
+  const classes = useStyles(); // ❌ If you have this, consider moving <ThemeProvider> to HOC and wrap the App
   return (
-    <>
-      <Grid container>
-        <Grid item xs={12} md={12}>
-          <MainNavigation></MainNavigation>
-        </Grid>
-        <Grid item xs={12} sm={12} md={3}>
-          <SecondNavigation mt={10}></SecondNavigation>
-          <LayoutMenber></LayoutMenber>
-        </Grid>
-        {/* <Grid item xs={12} sm={12} lg={3}>
+    <Grid container>
+      <Grid item xs={12} md={12}>
+        <MainNavigation></MainNavigation>
+      </Grid>
+      <Grid item xs={12} sm={12} md={3}>
+        <SecondNavigation mt={10}></SecondNavigation>
+        <LayoutMenber></LayoutMenber>
+      </Grid>
+      {/* <Grid item xs={12} sm={12} lg={3}>
           <HeaderProfile></HeaderProfile>
           <ListPhoto></ListPhoto>
         </Grid> */}
-        <Grid item xs={12} sm={12} md={6}>
-          <NewsFeed></NewsFeed>
-        </Grid>
-        <Grid item xs={12} sm={12} md={3}>
-          <LayoutListMember data={dataMember}></LayoutListMember>
-        </Grid>
+      <Grid item xs={12} sm={12} md={6}>
+        <NewsFeed></NewsFeed>
       </Grid>
-    </>
+      <Grid item xs={12} sm={12} md={3}>
+        <LayoutListMember data={dataMember}></LayoutListMember>
+      </Grid>
+    </Grid>
   );
 }
-
 export default App;
