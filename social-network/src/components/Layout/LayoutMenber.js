fd31e305f5bc78d695f664bbde39bce7b74a6ed4
vothikimyen108 <vothikinyen2k@gmail.com>
2021-08-18 19:14:13 +0700

    add animated

--- social-network/src/components/Layout/LayoutMenber.js
+++ social-network/src/components/Layout/LayoutMenber.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 import LayoutMenberStyles from "./LayoutMenberStyles";
-
+import { useSpring, animated, config } from "react-spring";
 export default function LayoutMenber() {
   const classes = LayoutMenberStyles();
+  const [flip, set] = useState(false);
+  const { number } = useSpring({
+    reset: true,
+    reverse: flip,
+    from: { number: 0 },
+    number: 30,
+    delay: 500,
+    onfig: config.molasses,
+    onRest: () => set(!flip),
+  });
 
   return (
     <div className={classes.layout}>
-      <h1>30</h1>
+      <animated.h1>{number.to((n) => n.toFixed())}</animated.h1>
       <p>Thành viên tham gia</p>
     </div>
   );
--- social-network/src/components/News/NewsList.js
+++ social-network/src/components/News/NewsList.js
@@ -1,21 +1,29 @@
 //item
 import NewsItem from "./NewsItem";
-
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 const NewsList = (props) => {
+  //animated
+  useEffect(() => {
+    AOS.init({ duration: 1000 });
+  }, []);
   return (
     <div>
       {props.data.map((item) => (
-        <NewsItem
-          key={item.id}
-          avatar={item.avatar}
-          name={item.name}
-          content={item.content}
-          totalLike={item.totalLike}
-          totalShare={item.totalShare}
-          totalComment={item.totalComment}
-          isExpanded={true}
-          isShowImg={true}
-        ></NewsItem>
+        <div data-aos="fade-right">
+          <NewsItem
+            key={item.id}
+            avatar={item.avatar}
+            name={item.name}
+            content={item.content}
+            totalLike={item.totalLike}
+            totalShare={item.totalShare}
+            totalComment={item.totalComment}
+            isExpanded={true}
+            isShowImg={true}
+          ></NewsItem>
+        </div>
       ))}
     </div>
   );
