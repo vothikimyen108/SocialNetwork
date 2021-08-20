0a3f3614b1770be3128986e66b701b1dedfc92c5
vothikimyen108 <vothikinyen2k@gmail.com>
2021-08-20 10:08:16 +0700

    add slice new img

--- social-network/src/components/Photos/Photo.js
+++ social-network/src/components/Photos/Photo.js
@@ -1,326 +1,100 @@
-// import Slider from "react-animated-slider";
-// import "react-animated-slider/build/horizontal.css";
-// import { makeStyles } from "@material-ui/core/styles";
-
-// import Grid from "@material-ui/core/Grid";
-// import Anh from "../../assets/ImgPost/anh1.jpg";
-
-// import Anh2 from "../../assets/ImgPost/anh3.jpg";
-// import Anh3 from "../../assets/ImgPost/anh4.jpg";
-// import NewsItem from "../News/NewsItem";
-// import CssBaseline from "@material-ui/core/CssBaseline";
-// import CloseIcon from "@material-ui/icons/Close";
-// import IconButton from "@material-ui/core/IconButton";
-
-// import ModalFull from "../UI/ModalFull";
-
-// const useStyles = makeStyles((theme) => ({
-//   root: {
-//     // backgroundColor: "#000",
-//     // height: "100vh",
-//     display: "flex",
-//     alignItems: "center",
-//     justifyContent: "center",
-//     // position: "relative",
-//   },
-
-//   img: {
-//     width: "auto",
-//     height: "100%",
-//   },
-//   left: {},
-//   right: {
-//     overflow: "scroll",
-//     width: "auto",
-//     height: "100vh",
-//   },
-//   button: {
-//     width: "40px",
-//     height: "40px",
-//     backgroundColor: "#e4e6eb",
-//     color: "#000",
-//     position: "absolute",
-//     left: "0",
-//     top: "10px",
-//     zIndex: 100,
-//     "&:hover": {
-//       backgroundColor: "#e4e6eb",
-//       color: "#000",
-//     },
-//   },
-// }));
-
-// const Photo = (props) => {
-//   const classes = useStyles();
-//   const slides = [{ img: `${Anh}` }, { img: `${Anh2}` }, { img: `${Anh3}` }];
-//   return (
-//     <ModalFull>
-//       <Grid container component="main" className={classes.root}>
-//         <CssBaseline />
-//         <Grid item xs={12} sm={12} lg={8} className={classes.left}>
-//           <IconButton
-//             className={classes.button}
-//             component="span"
-//             onClick={props.onClose}
-//           >
-//             <CloseIcon></CloseIcon>
-//           </IconButton>
-//           <Slider className="slider-wrapper">
-//             {slides.map((slide, index) => (
-//               <div key={index} className={classes.root}>
-//                 <img src={slide.img} className={classes.img} alt="anh"></img>
-//               </div>
-//             ))}
-//           </Slider>
-//         </Grid>
-//         <Grid
-//           item
-//           xs={12}
-//           sm={12}
-//           lg={4}
-//           elevation={6}
-//           square
-//           className={classes.right}
-//         >
-//           <NewsItem
-//             isOpenCMT={true}
-//             isExpanded={false}
-//             isShowImg={false}
-//             key={props.item.id}
-//             avatar={props.item.avatar}
-//             name={props.item.name}
-//             content={props.item.content}
-//             totalLike={props.item.totalLike}
-//             totalShare={props.item.totalShare}
-//             totalComment={props.item.totalComment}
-//           >
-//             {" "}
-//           </NewsItem>
-//         </Grid>
-//       </Grid>
-//     </ModalFull>
-//   );
-// };
-// export default Photo;
-import React from "react";
-import cx from "clsx";
+import Slider from "react-animated-slider";
+import "react-animated-slider/build/horizontal.css";
 import { makeStyles } from "@material-ui/core/styles";
-import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
-import KeyboardArrowRight from "@material-ui/icons/KeyboardArrowRight";
-import KeyboardArrowLeft from "@material-ui/icons/KeyboardArrowLeft";
-import ParallaxSlide from "@mui-treasury/components/slide/parallax";
-import DotIndicator from "@mui-treasury/components/indicator/dot";
-import { useArrowDarkButtonStyles } from "@mui-treasury/styles/button/arrowDark";
 
-const data = [
-  {
-    id: 1,
-    title: "Huarache",
-    subtitle: "Gripp",
-    image:
-      // eslint-disable-next-line max-len
-      "https://firebasestorage.googleapis.com/v0/b/mui-treasury.appspot.com/o/public%2Fshoes%2Fair-huarache-gripp.png?alt=media",
-  },
-  {
-    id: 2,
-    title: "Air Max",
-    subtitle: "270 P",
-    image:
-      // eslint-disable-next-line max-len
-      "https://firebasestorage.googleapis.com/v0/b/mui-treasury.appspot.com/o/public%2Fshoes%2Fair-max-270.png?alt=media",
-  },
-  {
-    id: 3,
-    title: "Air Max",
-    subtitle: "Deluxe",
-    image:
-      // eslint-disable-next-line max-len
-      "https://firebasestorage.googleapis.com/v0/b/mui-treasury.appspot.com/o/public%2Fshoes%2Fair-max-deluxe.png?alt=media",
-  },
-];
+import Grid from "@material-ui/core/Grid";
+import Anh from "../../assets/ImgPost/anh1.jpg";
 
-const useStyles = makeStyles(({ palette, breakpoints, spacing }) => ({
+import Anh2 from "../../assets/ImgPost/anh3.jpg";
+import Anh3 from "../../assets/ImgPost/anh4.jpg";
+import NewsItem from "../News/NewsItem";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import CloseIcon from "@material-ui/icons/Close";
+import IconButton from "@material-ui/core/IconButton";
+
+import ModalFull from "../UI/ModalFull";
+import ParallaxCarousel from "../UI/ParallaxCarousel";
+const useStyles = makeStyles((theme) => ({
   root: {
-    // a must if you want to set arrows, indicator as absolute
-    position: "relative",
-    width: "100%",
+    // backgroundColor: "#000",
+    // height: "100vh",
+    // display: "flex",
+    // alignItems: "center",
+    // justifyContent: "center",
+    // position: "relative",
   },
-  slide: {
-    perspective: 1000, // create perspective
-    overflow: "hidden",
-    // relative is a must if you want to create overlapping layers in children
-    position: "relative",
-    paddingTop: spacing(8),
-    [breakpoints.up("sm")]: {
-      paddingTop: spacing(10),
-    },
-    [breakpoints.up("md")]: {
-      paddingTop: spacing(14),
-    },
-  },
-  imageContainer: {
-    display: "block",
-    position: "relative",
-    zIndex: 2,
-    paddingBottom: "56.25%",
-  },
-  image: {
-    display: "block",
-    position: "absolute",
-    zIndex: 10,
-    width: "100%",
+
+  img: {
+    width: "auto",
     height: "100%",
-    objectFit: "cover",
-    marginLeft: "12%",
-    [breakpoints.up("sm")]: {
-      marginLeft: "4%",
-    },
   },
-  arrow: {
-    display: "none",
+  left: {
+    position: "relative",
+  },
+  right: {
+    overflow: "scroll",
+    width: "auto",
+    height: "100vh",
+  },
+  button: {
+    width: "40px",
+    height: "40px",
+    backgroundColor: "#e4e6eb",
+    color: "#000",
     position: "absolute",
-    top: "50%",
-    transform: "translateY(-50%)",
-    [breakpoints.up("sm")]: {
-      display: "inline-flex",
+    left: "0",
+    top: "10px",
+    zIndex: 100,
+    "&:hover": {
+      backgroundColor: "#e4e6eb",
+      color: "#000",
     },
-  },
-  arrowLeft: {
-    left: 0,
-    [breakpoints.up("lg")]: {
-      left: -64,
-    },
-  },
-  arrowRight: {
-    right: 0,
-    [breakpoints.up("lg")]: {
-      right: -64,
-    },
-  },
-  text: {
-    // shared style for text-top and text-bottom
-    fontFamily: "Poppins, san-serif",
-    fontWeight: 900,
-    position: "absolute",
-    color: palette.common.white,
-    padding: "0 8px",
-    transform: "rotateY(45deg)",
-    lineHeight: 1.2,
-    [breakpoints.up("sm")]: {
-      padding: "0 16px",
-    },
-    [breakpoints.up("md")]: {
-      padding: "0 24px",
-    },
-  },
-  title: {
-    top: 20,
-    left: "20%",
-    height: "40%",
-    fontSize: 40,
-    zIndex: 1,
-    background: "linear-gradient(0deg, rgba(255,255,255,0) 0%, #9c9c9c 100%)",
-    [breakpoints.up("sm")]: {
-      top: 40,
-      fontSize: 72,
-    },
-    [breakpoints.up("md")]: {
-      top: 52,
-      fontSize: 72,
-    },
-  },
-  subtitle: {
-    top: 60,
-    left: "0%",
-    height: "52%",
-    fontSize: 56,
-    zIndex: 2,
-    background: "linear-gradient(0deg, rgba(255,255,255,0) 0%, #888888 100%)",
-    [breakpoints.up("sm")]: {
-      top: 112,
-      left: "6%",
-      fontSize: 96,
-    },
-    [breakpoints.up("md")]: {
-      top: 128,
-      fontSize: 104,
-    },
-  },
-  indicatorContainer: {
-    textAlign: "center",
   },
 }));
 
-const ParallaxCarousel = () => {
+const Photo = (props) => {
   const classes = useStyles();
-  const arrowStyles = useArrowDarkButtonStyles();
-  const createStyle = (slideIndex, fineIndex) => {
-    const diff = slideIndex - fineIndex;
-    if (Math.abs(diff) > 1) return {};
-    return {
-      transform: `rotateY(${(-diff + 1) * 45}deg)`,
-    };
-  };
-  // eslint-disable-next-line react/prop-types
-  const renderElements = ({ index, onChangeIndex }) => (
-    <>
-      <Button
-        className={cx(classes.arrow, classes.arrowLeft)}
-        classes={arrowStyles}
-        disabled={index === 0}
-        onClick={() => onChangeIndex(index - 1)}
-      >
-        <KeyboardArrowLeft />
-      </Button>
-      <Button
-        className={cx(classes.arrow, classes.arrowRight)}
-        classes={arrowStyles}
-        disabled={index === data.length - 1}
-        onClick={() => onChangeIndex(index + 1)}
-      >
-        <KeyboardArrowRight />
-      </Button>
-      <div className={classes.indicatorContainer}>
-        {data.map(({ id }, i) => (
-          <DotIndicator
-            key={id}
-            active={i === index}
-            onClick={() => onChangeIndex(i)}
-          />
-        ))}
-      </div>
-    </>
-  );
-  const renderChildren = ({ injectStyle, fineIndex }) =>
-    data.map(({ id, title, subtitle, image }, i) => (
-      <div key={id} className={classes.slide}>
-        <Typography
-          noWrap
-          className={cx(classes.text, classes.title)}
-          style={{ ...injectStyle(i, 60), ...createStyle(i, fineIndex) }}
-        >
-          {title}
-        </Typography>
-        <Typography
-          noWrap
-          className={cx(classes.text, classes.subtitle)}
-          style={{ ...injectStyle(i, 40), ...createStyle(i, fineIndex) }}
-        >
-          {subtitle}
-        </Typography>
-        <div className={classes.imageContainer}>
-          <img className={classes.image} src={image} alt={"slide"} />
-        </div>
-      </div>
-    ));
+  const slides = [{ img: `${Anh}` }, { img: `${Anh2}` }, { img: `${Anh3}` }];
   return (
-    <div className={classes.root}>
-      <ParallaxSlide renderElements={renderElements}>
-        {renderChildren}
-      </ParallaxSlide>
-    </div>
+    <ModalFull>
+      <Grid container component="main" className={classes.root}>
+        <CssBaseline />
+        <Grid item xs={12} sm={12} lg={8} className={classes.left}>
+          <IconButton
+            className={classes.button}
+            component="span"
+            onClick={props.onClose}
+          >
+            <CloseIcon></CloseIcon>
+          </IconButton>
+          <ParallaxCarousel></ParallaxCarousel>
+        </Grid>
+        <Grid
+          item
+          xs={12}
+          sm={12}
+          lg={4}
+          elevation={6}
+          square
+          className={classes.right}
+        >
+          <NewsItem
+            isOpenCMT={true}
+            isExpanded={false}
+            isShowImg={false}
+            key={props.item.id}
+            avatar={props.item.avatar}
+            name={props.item.name}
+            content={props.item.content}
+            totalLike={props.item.totalLike}
+            totalShare={props.item.totalShare}
+            totalComment={props.item.totalComment}
+          >
+            {" "}
+          </NewsItem>
+        </Grid>
+      </Grid>
+    </ModalFull>
   );
 };
-
-export default ParallaxCarousel;
+export default Photo;
